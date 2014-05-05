318e4bf9cb89de2d09f5c1392b49a1078bd40e59
Roderic Day <roderic.day@gmail.com>
2014-05-05 09:29:49 -0700

    Handle `return`

--- minipnm/utils.py
+++ minipnm/utils.py
@@ -1,6 +1,6 @@
 import inspect, time, warnings
 
-IGNORE = ['def ', 'for ', '#', '@', 'return ', 'print ']
+IGNORE = ['def ', 'for ', '#', '@', 'print ', 'print(']
 
 def profile(external_globals=None):
 
@@ -15,16 +15,23 @@ def profile(external_globals=None):
         psl_append = lambda string: profiled_source_lines.append(indent+string)
         for indented_line in source[1:]:
             line = indented_line.strip()
+
             if not line or any(line.startswith(i) for i in IGNORE):
                 profiled_source_lines.append(indented_line)
                 continue
 
             indent = ' '*(len(indented_line)-len(line))
             psl_append("start = time.time()")
-            psl_append(line)
+
+            if line.startswith("return "):
+                psl_append(line.replace("return ", "out = "))
+            else:
+                psl_append(line)
             psl_append("time_taken = 1000*(time.time()-start)")
             psl_append("print( '{}{{time_taken:>10.0f}}ms'.format(**locals()) )"\
                        .format(indented_line.ljust(padded_length)))
+            if line.startswith("return "):
+                psl_append("return out")
         
         profiled_source = '\n'.join(profiled_source_lines)
         try:
