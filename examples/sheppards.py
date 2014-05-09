48f2b4c3234dc6efb60630efcbd09a07a5c65881
Roderic Day <roderic.day@gmail.com>
2014-05-09 16:31:11 -0700

    More reshuffling of example files

--- examples/sheppards.py
+++ examples/sheppards.py
@@ -1,19 +1,18 @@
 import gzip
 import numpy as np
-from scipy.ndimage import zoom
+from scipy import ndimage
 import minipnm as mini
 
 path = 'segmented_bead_pack_512.ubc.gz'
 string = gzip.open(path).read()
 im = np.fromstring(string, dtype='int8').reshape(512,512,512)
 im = im[:-1,:-1,:-1]
-im = zoom(im, 0.4, order=1)[:,:,:5]
-print im.shape
+im = ndimage.zoom(im, 0.2, order=1)
+im = im[:,:,5]
+im = ndimage.morphology.distance_transform_bf(im)
 
+print im
 network = mini.Cubic(im, im.shape)
-balls = network - (im==0)
-network = network - (im==1)
-x,y,z = network.coords
-ics = 3*(x==x.min()) + 1*(x==x.max())
-network['sol'] = mini.linear_solve(network, ics)
-network.render(network['sol'])
+void = network - (im==0)
+structure = network - (im==1)
+void.render(void['intensity'], alpha=1)
