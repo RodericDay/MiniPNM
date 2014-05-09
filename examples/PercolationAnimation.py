Fixed segmentation example. Involved normalization in graphics.

examples/PercolationAnimation.py
@@ -2,12 +2,12 @@ import numpy as np
 import minipnm as mini
 
 network = mini.Cubic.empty([30,10,10])
-# network = mini.Delaunay(network.points)
+network = mini.Delaunay(network.points)
 x,y,z = network.coords
 
 source = x == x.min()
-threshold = np.random.rand(network.size[0])
-conditions = np.logspace(np.log10(threshold.min()), np.log10(threshold.max()*3), 100)
+threshold = x*np.random.rand(network.size[0])
+conditions = np.linspace(threshold.min(), threshold.max()*3, 100)
 
 saturations = mini.percolation(network, source, threshold, conditions)
 

minipnm/algorithms.py
@@ -4,7 +4,7 @@ from scipy.sparse.linalg import spsolve
 
 def linear_solve(network, dbcs):
     # the matrix to be solved will involve a stack of ICs and some balances
-    # take as ICs points where dbcs is defined
+    # take as fixed the points where dbcs is defined
     fixed = dbcs!=0
 
     # if there are unconnected islands, they need to be fixed at zero too

minipnm/graphics.py
@@ -22,12 +22,12 @@ def vcolors(values, cmap='coolwarm'):
     colors = vtk.vtkUnsignedCharArray()
     colors.SetNumberOfComponents(3)
     colormap = cm.get_cmap(cmap)
-    mapped = colormap(values.astype(float))
+    mapped = colormap(values)
     for r,g,b,a in 255*mapped:
         colors.InsertNextTuple3(r,g,b)
     return colors
 
-def wires(network, alpha=0.5):
+def wires(network, alpha=0.2):
     polydata = vtk.vtkPolyData()
     polydata.SetPoints(vpoints(network))
     polydata.SetLines(vpolys(network))
@@ -62,9 +62,9 @@ def animate(iren, timeout, rate=1):
     iren.AddObserver('TimerEvent', timeout)
     timer = iren.CreateRepeatingTimer(rate)
 
-def render(network, values=None, rate=1):
+def render(network, values=None, rate=1, alpha=0.2):
     ren = vtk.vtkRenderer()
-    wires_actor = wires(network)
+    wires_actor = wires(network, alpha)
     ren.AddActor(wires_actor)
 
     renWin = vtk.vtkRenderWindow()
@@ -74,6 +74,8 @@ def render(network, values=None, rate=1):
     iren.Initialize()
 
     view2d = np.atleast_2d(values) # cuts down on a lot of error-checking
+    view2d = np.subtract(view2d, view2d.min())
+    view2d = np.true_divide(view2d, view2d.max())
     counter = {'value': 0} # find a better way to get around namespace issues
     
     def timeout(object, event):

minipnm/network.py
@@ -202,11 +202,8 @@ class Cubic(Network):
         return cls(arr, shape or arr.shape)
 
     def __init__(self, ndarray, dims=[1,1,1]):
-
-        # handle 2D array (image) input
-        if len(ndarray.shape) == 2:
-            dims = ndarray.shape + (1,)
-            ndarray = ndarray.reshape(dims)
+        ndarray = np.atleast_3d(ndarray)
+        dims = tuple(dims) + (1,) * (3 - len(dims))
 
         self['intensity'] = ndarray.ravel()
 