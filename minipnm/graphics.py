c5c7bd39ad5becd5ccfb4a4f617715a8792b816b
Roderic Day <roderic.day@gmail.com>
2014-06-24 18:05:22 -0700

    Refactored graphics to isolate actors better, but broke the tube method

--- minipnm/graphics.py
+++ minipnm/graphics.py
@@ -1,187 +1,195 @@
-from __future__ import print_function, division
-import os
-from itertools import cycle
+from itertools import count
 import numpy as np
 from matplotlib import cm
 import vtk
+import minipnm as mini
 
-from .misc import normalize
 
-def vscalars(data):
-    dataArray = vtk.vtkFloatArray()
-    for i in data:
-        dataArray.InsertNextValue(i)
-    return dataArray
+class Actor(vtk.vtkActor):
 
-def vpoints(coords):
-    points = vtk.vtkPoints()
-    for x,y,z in coords:
-        points.InsertNextPoint(x, y, z)
-    return points
+    def __init__(self):
+        raise NotImplementedError()
 
-def vpolys(pairs):
-    polys = vtk.vtkCellArray()
-    for hi, ti in pairs:
-        vil = vtk.vtkIdList()
-        vil.InsertNextId(hi)
-        vil.InsertNextId(ti)
-        polys.InsertNextCell(vil)
-    return polys
+    def pointArray(self, vertex_coords):
+        points = vtk.vtkPoints()
+        for x,y,z in vertex_coords:
+            points.InsertNextPoint(x, y, z)
+        return points
 
-def vcolors(values, cmap='coolwarm'):
-    colors = vtk.vtkUnsignedCharArray()
-    colors.SetNumberOfComponents(3)
-    colormap = cm.get_cmap(cmap)
-    mapped = colormap(values)
-    for r,g,b,a in 255*mapped:
-        colors.InsertNextTuple3(r,g,b)
-    return colors
+    def lineArray(self, edge_pairs):
+        lines = vtk.vtkCellArray()
+        for t,h in edge_pairs:
+            l = vtk.vtkIdList()
+            l.InsertNextId(t)
+            l.InsertNextId(h)
+            lines.InsertNextCell(l)
+        return lines
+
+    def floatArray(self, array):
+        floats = vtk.vtkFloatArray()
+        for n in array:
+            floats.InsertNextValue(n)
+        return floats
+
+    def colorArray(self, array, cmap=None):
+        colors = vtk.vtkUnsignedCharArray()
+        colors.SetNumberOfComponents(3)
+        if cmap is None:
+            cmap = 'copper'
+        colormap = cm.get_cmap(cmap)
+        mapped = colormap(array)
+        for r,g,b,a in 255*mapped:
+            colors.InsertNextTuple3(r,g,b)
+        return colors
+
+    def update(self, t):
+        pass
+
+
+class Wires(Actor):
+
+    def __init__(self, vertex_coords, edge_pairs, vertex_weights=None, alpha=1, cmap=None):
+        self.polydata = vtk.vtkPolyData()
+        self.polydata.SetPoints(self.pointArray(vertex_coords))
+        self.polydata.SetLines(self.lineArray(edge_pairs))
+        self.mapper = vtk.vtkPolyDataMapper()
+        self.mapper.SetInput(self.polydata)
+        self.SetMapper(self.mapper)
+
+        if vertex_weights is None:
+            weights = np.atleast_2d([256 for _ in vertex_coords])
+        else:
+            weights = np.atleast_2d(vertex_weights)
+            weights = np.subtract(weights, weights.min())
+            weights = np.true_divide(weights, weights.max())
+        self.weights = weights
+        self.cmap = cmap
+
+        self.GetProperty().SetOpacity(alpha)
+
+    def update(self, t=0):
+        i = t % len(self.weights)
+        self.polydata.GetPointData().SetScalars(self.colorArray(self.weights[i], self.cmap))
+
+
+class Spheres(Actor):
+
+    def __init__(self, centers, radii=None, alpha=1, color=(1,1,1)):
+        self.polydata = vtk.vtkPolyData()
+        self.polydata.SetPoints(self.pointArray(centers))
+        self.radii = np.atleast_2d(radii)
+        self.update()
+
+        self.sphere_source = vtk.vtkSphereSource()
+        self.glypher = vtk.vtkProgrammableGlyphFilter()
+        self.glypher.SetInput(self.polydata)
+        self.glypher.SetSource(self.sphere_source.GetOutput())
+        self.glypher.SetGlyphMethod(self.glyph_method)
+
+        self.mapper = vtk.vtkPolyDataMapper()
+        self.mapper.SetInputConnection(self.glypher.GetOutputPort())
+        self.SetMapper(self.mapper)
+
+        self.GetProperty().SetOpacity(alpha)
+        r,g,b = color
+        self.mapper.SetScalarVisibility(False)
+        self.GetProperty().SetColor(r,g,b)
+
+    def glyph_method(self):
+        pid = self.glypher.GetPointId()
+        self.sphere_source.SetCenter(self.glypher.GetPoint())
+        radius = self.glypher.GetPointData().GetScalars().GetValue(pid)
+        self.sphere_source.SetRadius(radius)
+
+    def update(self, t=0):
+        i = t % len(self.radii)
+        self.polydata.GetPointData().SetScalars(self.floatArray(self.radii[i]))
 
 
 class Scene(object):
+    ticks = count(0)
 
-    def __init__(self, size=(800,600)):
+    def __init__(self, fix_camera=True):
+        '''
+        fix_camera : more sensible default
+        '''
+        # create
         self.ren = vtk.vtkRenderer()
-        self.style = vtk.vtkInteractorStyleTrackballCamera()
-        self.animation_batch = []
+        self.renWin = vtk.vtkRenderWindow()
+        self.iren = vtk.vtkRenderWindowInteractor()
 
-    def map_and_append(fn):
-        '''
-        this decorator handles some boilerplate code regarding actor addition
-        '''
-        def handler(self, *args, **kwargs):
-            alpha = kwargs.pop('alpha', 1)
-            color = kwargs.pop('color', None)
+        # hookups
+        self.renWin.AddRenderer(self.ren)
+        self.iren.SetRenderWindow(self.renWin)
+        
+        if fix_camera:
+            camera = vtk.vtkInteractorStyleTrackballCamera()
+            self.iren.SetInteractorStyle(camera)
 
-            polydata = fn(self, *args, **kwargs)
-            mapper = vtk.vtkPolyDataMapper()
-            try:
-                mapper.SetInput(polydata)
-            except TypeError:
-                mapper.SetInputConnection(polydata.GetOutputPort())
-            actor = vtk.vtkActor()
-            actor.SetMapper(mapper)
-            actor.GetProperty().SetOpacity(alpha)
-            if color is not None:
-                mapper.SetScalarVisibility(False)
-                actor.GetProperty().SetColor(*color)
-            self.ren.AddActor(actor)
-
-        return handler
-
-    @map_and_append
-    def add_wires(self, points, pairs, history=None, cmap='coolwarm'):
-        polydata = vtk.vtkPolyData()
-        polydata.SetPoints(vpoints(points))
-        polydata.SetLines(vpolys(pairs))
-
-        if history is not None:
-            looper = cycle(np.atleast_2d(normalize(history)))
-            self.animation_batch.append(
-                lambda: polydata.GetPointData().SetScalars(
-                        vcolors(next(looper), cmap))
-            )
-
-        return polydata
-
-    @map_and_append
-    def add_tubes(self, points, pairs, history=None, radius=None, detail=5,
-                  cmap='coolwarm'):
-        polydata = vtk.vtkPolyData()
-        polydata.SetPoints(vpoints(points))
-        polydata.SetLines(vpolys(pairs))
-
-        if history is not None:
-            looper = cycle(np.atleast_2d(normalize(history)))
-            self.animation_batch.append(
-                lambda: polydata.GetPointData().SetScalars(
-                        vcolors(next(looper), cmap))
-            )
-
-        if radius is None:
-            # we don't want chunky tubes
-            lmin = np.linalg.norm(np.diff(points[pairs], axis=1), axis=2).min()
-            radius = lmin/10
-
-        tubeFilter = vtk.vtkTubeFilter()
-        tubeFilter.SetInput(polydata)
-        tubeFilter.SetRadius(radius)
-        tubeFilter.SetNumberOfSides(detail)
-        return tubeFilter
-
-    @map_and_append
-    def add_spheres(self, points, radii):
-        '''
-        consists of centers and radii
-        '''
-        polydata = vtk.vtkPolyData()
-        polydata.SetPoints(vpoints(points))
-
-        try:
-            assert len(points) == np.array(radii).shape[-1]
-        except IndexError:
-            # probably a scalar, so broadcast
-            radii = np.ones(len(points))*radii
-        looper = cycle(np.atleast_2d(radii))
-        self.animation_batch.append(
-            lambda: polydata.GetPointData().SetScalars(
-                    vscalars(next(looper)))
-        )
-
-        def Glyph():
-            pid = glypher.GetPointId()
-            sphere.SetCenter(glypher.GetPoint())
-            try:
-                value = glypher.GetPointData().GetScalars().GetValue(pid)
-                sphere.SetRadius(value)
-            except AttributeError:
-                pass
-
-        sphere = vtk.vtkSphereSource()
-        glypher = vtk.vtkProgrammableGlyphFilter()
-        glypher.SetInput(polydata)
-        glypher.SetSource(sphere.GetOutput())
-        glypher.SetGlyphMethod(Glyph)
-
-        return glypher
-
-    def timeout(self, object=None, event=None):
-        for fn in self.animation_batch:
-            fn()
-        self.ren.ResetCameraClippingRange()
+    def update_all(self, object=None, event=None):
+        for aid in range(self.ren.VisibleActorCount()):
+            actor = self.ren.GetActors().GetItemAsObject(aid)
+            actor.update(next(self.ticks))
         self.renWin.Render()
 
-    def play(self, rate=1, size=(800,600)):
-        self.renWin = vtk.vtkRenderWindow()
-        self.renWin.SetSize(*size)
-        self.renWin.AddRenderer(self.ren)
-        self.iren = vtk.vtkRenderWindowInteractor()
-        self.iren.SetRenderWindow(self.renWin)
-        self.iren.SetInteractorStyle(self.style)
+    def play(self, timeout=1):
         self.iren.Initialize()
-        self.iren.AddObserver('TimerEvent', self.timeout)
-
-        if rate:
-            timer = self.iren.CreateRepeatingTimer(rate)
-        self.timeout()
+        if timeout is not None:
+            self.iren.AddObserver('TimerEvent', self.update_all)
+            self.timer = self.iren.CreateRepeatingTimer(timeout)
+        self.update_all()
         self.iren.Start()
+        
+    def add_actor(self, actor):
+        self.ren.AddActor(actor)
 
-    def save(self, size=(400,300), frames=1):
-        self.renWin.SetSize(*size)
-        w2if = vtk.vtkWindowToImageFilter()
-        w2if.SetInput(self.renWin)
-         
-        writer = vtk.vtkPNGWriter()
-        try:
-            os.system("mkdir tmp")
+    # legacy functions
+    def add_wires(self, points, pairs, weights=None, alpha=1, cmap=None):
+        wires = Wires(points, pairs, weights, alpha, cmap)
+        self.add_actor(wires)
 
-            for i in range(frames):
-                self.timeout()
-                w2if.Modified()
-                writer.SetFileName("tmp/{:0>3}.png".format(i))
-                writer.SetInput(w2if.GetOutput())
-                writer.Write()
+    def add_tubes(self):
+        pass
 
-            os.system("convert -delay 20 -loop 0 ./tmp/*.png ~/animated.gif")
-        finally:
-            os.system("rm -r tmp")
+    def add_spheres(self, points, radii, alpha=1, color=(1,1,1)):
+        spheres = Spheres(points, radii, alpha, color)
+        self.add_actor(spheres)
+
+
+if __name__ == '__main__':
+    network = mini.Delaunay.random(100)
+    x,y,z = network.coords
+    dbcs = 2*(x==x.min()) + 1*(x==x.max())
+    sol = mini.linear_solve(network, dbcs)
+
+    wires = Wires(network.points, network.pairs, sol)
+    shells = Spheres(network.points, [0.08*1.05 for _ in network.points], alpha=0.3, color=(1,1,1))
+    spheres = Spheres(network.points, [[r for _ in network.points] for r in np.linspace(0.01,0.08,50)], color=(0,0,1))
+
+    scene = Scene()
+    scene.add_actor(wires)
+    scene.add_actor(shells)
+    scene.add_actor(spheres)
+    scene.play(100)
+
+
+#~~
+def save_gif(self, size=(400,300), frames=1):
+    self.renWin.SetSize(*size)
+    w2if = vtk.vtkWindowToImageFilter()
+    w2if.SetInput(self.renWin)
+     
+    writer = vtk.vtkPNGWriter()
+    try:
+        os.system("mkdir tmp")
+
+        for i in range(frames):
+            self.timeout()
+            w2if.Modified()
+            writer.SetFileName("tmp/{:0>3}.png".format(i))
+            writer.SetInput(w2if.GetOutput())
+            writer.Write()
+
+        os.system("convert -delay 20 -loop 0 ./tmp/*.png ~/animated.gif")
+    finally:
+        os.system("rm -r tmp")
