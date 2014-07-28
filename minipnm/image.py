Minor changes, fixing argument sending and noise normalization

minipnm/image.py
@@ -17,8 +17,8 @@ def gaussian_noise(shape, exp=1, mode='wrap'):
     N = np.zeros_like(R)
     for sigma in 2**np.arange(6):
         N += ndimage.filters.gaussian_filter(R, sigma, mode=mode) * sigma**exp
-    N = np.true_divide(N, np.abs(N).max())
     N = np.subtract(N, N.min())
+    N = np.true_divide(N, np.abs(N).max())
     return N
 
 def save_gif(image_stack, outfile='animated.gif'):

minipnm/network.py
@@ -128,7 +128,7 @@ class Network(dict):
         if not wait:
             scene.play()
 
-    def actors(self, values=None):
+    def actors(self, values=None, **kwargs):
         try:
             # to load as if given a key
             values = self[values]
@@ -141,7 +141,7 @@ class Network(dict):
             # probably an array, but make sure it fits!
             assert np.array(values).shape[-1] == self.order
         finally:
-            wires = graphics.Wires(self.points, self.pairs, values)
+            wires = graphics.Wires(self.points, self.pairs, values, **kwargs)
         return [wires]
 
     def merge(self, other, axis=2, spacing=None, centering=False, stitch=False):