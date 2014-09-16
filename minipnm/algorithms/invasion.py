959450722fdb39c7363b651aedaa395c56dfac3c
Roderic Day <roderic.day@gmail.com>
2014-09-16 13:57:51 -0700

    Simultaneous flooding algorithm added

--- minipnm/algorithms/invasion.py
+++ minipnm/algorithms/invasion.py
@@ -1,6 +1,10 @@
 import numpy as np
+from scipy import sparse
 
 def invasion(adjacency_matrix, sources, sinks=None):
+    '''
+    Most basic textbook example of sequential invasion
+    '''
     tails = adjacency_matrix.row
     heads = adjacency_matrix.col
     conductance = adjacency_matrix.data
@@ -27,3 +31,57 @@ def invasion(adjacency_matrix, sources, sinks=None):
         history.append(state.copy())
 
     return np.vstack(history)
+
+
+class InvasionSimulation(object):
+    '''
+    This class simulates alop invasion with fractional generation in arbitrary
+    pores simultaneously
+    '''
+    def __init__(self, capacities, conductance_matrix):
+        self.capacities = capacities
+        self.con = conductance_matrix
+        self.saturation = np.zeros_like(capacities)
+
+    def distribute(self, generation):
+        content = self.capacities*self.saturation + generation
+        excess = content.clip(self.capacities, content) - self.capacities
+        content -= excess
+        self.saturation = content / self.capacities
+        if any(excess):
+            for node in excess.nonzero()[0]:
+                recipient = self.find_unsaturated_neighbor(node)
+                if node == recipient:
+                    raise Exception("Infinite loop")
+                    return self.saturation
+                excess[recipient] += excess[node]
+                excess[node] = 0
+            return self.distribute(excess)
+        return self.saturation
+
+    def find_unsaturated_neighbor(self, node):
+        self.update_pressurized_clusters()
+        full_sources = self.labels[self.con.col]==self.labels[node]
+        non_full_sinks = self.saturation[self.con.row] < 0.999
+        viable_throats = full_sources & non_full_sinks
+        viable_throats = viable_throats.nonzero()[0] # bool -> idxs
+        if len(viable_throats) == 0:
+            return node
+        best_throat = max(viable_throats, key=self.con.data.item)
+        neighbor = self.con.row[best_throat]
+        return neighbor
+
+    def update_pressurized_clusters(self):
+        '''
+        this only needs to be checked when pores are newly saturated
+        '''
+        full_nodes = (self.saturation > 0.999).nonzero()[0]
+        # superclusters require pressurized throats
+        pressurized_throats = np.in1d(self.con.row, full_nodes) \
+                            & np.in1d(self.con.col, full_nodes)
+        i = self.con.row[pressurized_throats]
+        j = self.con.col[pressurized_throats]
+        v = np.ones_like(i)
+        s = self.capacities.size
+        coo = sparse.coo_matrix((v, (i, j)), shape=(s,s))
+        self.labels = sparse.csgraph.connected_components(coo)[1]
